e0ce8706d5fd2c3351fa5d18eb0a928d54ce550d
daenuli <giarsyani.nuli@gmail.com>
2022-11-22 09:43:27 +0700

    update email

--- resources/views/emails/auth/verification.blade.php
+++ resources/views/emails/auth/verification.blade.php
@@ -1,17 +1,13 @@
-@component('mail::message')
-# Verifikasi Email Pendaftaran
+Verifikasi Email Pendaftaran
+<br>
+Terimakasih anda telah mendaftar di bolasoft.id, silahkan klik button dibawah ini untuk meng-aktivasi akun anda :
 
-Terimakasih anda telah mendaftar di bolasoft.id, silahkan klik button dibawah ini untuk meng-aktivasi akun anda : 
-    
-
-@component('mail::button', ['url' => $url])
-Aktivasi Sekarang
-@endcomponent
-
+<br>
+<a href="{{$url}}">Aktivasi Sekarang</a>
+<br>
 
 Demikian informasi dari kami, semoga bermanfaat bagi anda<br>
 Terimakasih.<br><br>
 Hormat Kami,<br>
 <hr>
 {{ env('APP_NAME') }} Indonesia
-@endcomponent
