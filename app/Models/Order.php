35f536ba763ef309abf1b6fcf8aec2b822f735cc
daenuli <giarsyani.nuli@gmail.com>
2022-11-25 13:49:25 +0700

    update order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -5,5 +5,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    
+    protected $fillable = ['user_id', 'number', 'total_price', 'payment_status', 'snap_token', 'redirect_url', 'response_midtrans'];
 }
